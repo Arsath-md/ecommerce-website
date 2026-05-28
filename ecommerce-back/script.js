fc357bde2554ac21f85cce49ce22080fa79ae5a3
Arsath-md <mdarsath.m2003@gmail.com>
2026-05-28 20:24:37 +0530

    bismillah

--- ecommerce-back/script.js
+++ ecommerce-back/script.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 app.use(express.json());
 app.use(parser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ecommerce-website-7662.onrender.com",
     credentials: true
 }));
 
