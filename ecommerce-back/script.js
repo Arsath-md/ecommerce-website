6b3263640518fcddd92dbf91e6c99c5f0676d43b
Arsath-md <mdarsath.m2003@gmail.com>
2026-05-29 10:48:29 +0530

    bismillah

--- ecommerce-back/script.js
+++ ecommerce-back/script.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const dotenv = require("dotenv")
 dotenv.config()
-
+const path = require("path")
+const {filetopath}= require("url")
 const app = express();
 const moongo = require("mongoose")
 const parser = require("cookie-parser")
@@ -13,6 +14,17 @@ app.use(cors({
     origin:"https://ecommerce-s74m.onrender.com",
     credentials: true
 }));
+
+const __filename = fileURLToPath("https://ecommerce-s74m.onrender.com/")
+const __dirname = path.dirname(__filename)
+
+// Frontend build folder
+app.use(express.static(path.join(__dirname, "dist")))
+
+// IMPORTANT
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"))
+})
 
 moongo.connect(process.env.MONGO)
 .then(
